cb93da37852b7c09acfec70cf429ce0ba271b615
Marcio <devmarciofaria@gmail.com>
2022-10-12 23:02:00 -0300

    first route

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,9 @@ const server_port = process.env.SERVER_PORT
 
 const app = express();
 
+app.get("/", (request, response)=>{
+  return response.json({message:"Hello World"});
+})
+
+
 app.listen(server_port, ()=> console.log(`Server is Running on port ${server_port} 🍷🗿`))
