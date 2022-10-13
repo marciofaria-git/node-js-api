b830f4c501a6e6413909b4e38181f291fedfa795
Marcio <devmarciofaria@gmail.com>
2022-10-12 22:57:14 -0300

    create running and env config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const server_port = process.env.SERVER_PORT
 
 const app = express();
 
-app.listen(server_port, ()=> console.log("Server is running!"))
+app.listen(server_port, ()=> console.log(`Server is Running on port ${server_port} 🍷🗿`))
