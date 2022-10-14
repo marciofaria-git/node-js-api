be70edb2c0ff280a5c3e803b2a05d8197d4c1cfd
Marcio <devmarciofaria@gmail.com>
2022-10-13 22:51:27 -0300

    create route courses and debbug

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,17 @@ const server_port = process.env.SERVER_PORT
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (request, response)=>{
   return response.json({message:"Hello World"});
 })
+
+app.post("/courses", (request,response)=>{
+  const {name} = request.body;
+  return response.json({name});
+})
+ 
 
 
 app.listen(server_port, ()=> console.log(`Server is Running on port ${server_port} 🍷🗿`))
